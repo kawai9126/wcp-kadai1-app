743fa5f737ac26e0920db2e677072757c311cd9b
kawai9126 <iroha.9126@gmail.com>
2021-05-08 09:41:05 +0000

    @edit~delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
@@ -9,6 +10,17 @@ class BooksController < ApplicationController
      @books = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
   def edit
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
