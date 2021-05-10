7698aba86beabc3cc7456167bc1e693aa3d4565d
kawai9126 <iroha.9126@gmail.com>
2021-05-10 04:41:55 +0000

    delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path(book.id)
+    redirect_to book_path(book.id)
   end
 
   def edit
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
 
   private
