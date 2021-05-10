d7ec441497fca329cbfbd70125e6a0c98bda9f0c
kawai9126 <iroha.9126@gmail.com>
2021-05-10 09:12:55 +0000

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
 
+  get 'book/index'
+  get 'home/index'
+  root to: 'book#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :books
 resources :homes
