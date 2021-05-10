1263fe5e72474c9ccdb8049952a7b5564158ff1d
kawai9126 <iroha.9126@gmail.com>
2021-05-10 08:59:14 +0000

    successfuly

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: 'successfully'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -27,7 +30,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'book was successfully destroyed.'
   end
 
   private
